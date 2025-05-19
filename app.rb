d8ff256d9d77ccecc3b6e69d21f886089cbfba07
Karen Barbé <info@karenbarbe.com>
2025-05-19 15:52:28 -0500

    add hex color validation

--- app.rb
+++ app.rb
@@ -2,45 +2,59 @@ require "sinatra"
 require "sinatra/reloader"
 require "http"
 require "json"
+require_relative "validate_hex"
 
 get("/") do
   @fg_color = "d473c0"
-  @bg_color = "f4f4f1"
+  @bg_color = "511c29"
+  @original_fg_color = "d473c0"
+  @original_bg_color = "511c29"
+  @error_message = nil
 
-  fg_api_url = "https://www.thecolorapi.com/id?hex=#{@fg_color}"
-  fg_api_response = HTTP.get(fg_api_url)
-  fg_api_parsed = JSON.parse(fg_api_response)
-  bg_api_url = "https://www.thecolorapi.com/id?hex=#{@bg_color}"
-  bg_api_response = HTTP.get(bg_api_url)
-  bg_api_parsed = JSON.parse(bg_api_response)
-
-  @fg_name = fg_api_parsed["name"]["value"]
-  @fg_contrast_value = fg_api_parsed["contrast"]["value"]
-  @bg_name = bg_api_parsed["name"]["value"]
-  @bg_contrast_value = bg_api_parsed["contrast"]["value"]
-
-  webaim_api_url = "https://webaim.org/resources/contrastchecker/?fcolor=#{@fg_color}&bcolor=#{@bg_color}&api"
-  webaim_api_response = HTTP.get(webaim_api_url)
-  webaim_api_parsed = JSON.parse(webaim_api_response)
-
-  @ratio = webaim_api_parsed["ratio"].to_f
-  @aa = webaim_api_parsed["AA"].to_s
-  @aa_large = webaim_api_parsed["AALarge"].to_s
-  @aaa = webaim_api_parsed["AAA"].to_s
-  @aaa_large = webaim_api_parsed["AAALarge"].to_s
+  request_colors_apis(@fg_color, @bg_color)
   
-
   erb(:home)
 end
 
 get("/check_contrast") do
-  @fg_color = params.fetch("foreground_color")
-  @bg_color = params.fetch("background_color")
+  raw_fg_color = params.fetch("foreground_color").gsub("#", "")
+  raw_bg_color = params.fetch("background_color").gsub("#", "")
 
-  fg_api_url = "https://www.thecolorapi.com/id?hex=#{@fg_color}"
+  # Check if foreground color is a valid hex color
+  if validate_hex(raw_fg_color)
+    @fg_color = raw_fg_color
+    @original_fg_color = raw_fg_color
+    @fg_error_message = nil
+  else
+    @fg_color = "000000"
+    @original_fg_color = raw_fg_color
+    @fg_error_message = "Oops! Invalid hex code"
+  end
+
+  # Check if background color is a valid hex color
+  if validate_hex(raw_bg_color)
+    @bg_color = raw_bg_color
+    @original_bg_color = raw_bg_color
+    @bg_error_message = nil
+  else
+    @bg_color = "ffffff"
+    @original_bg_color = raw_bg_color
+    @bg_error_message = "Oops! Invalid hex code"
+  end
+
+
+  request_colors_apis(@fg_color, @bg_color)
+
+  erb(:home)
+end
+
+
+def request_colors_apis(fg_color, bg_color)
+  # Request color api to get hex color name and corresponding contrast value
+  fg_api_url = "https://www.thecolorapi.com/id?hex=#{fg_color}"
   fg_api_response = HTTP.get(fg_api_url)
   fg_api_parsed = JSON.parse(fg_api_response)
-  bg_api_url = "https://www.thecolorapi.com/id?hex=#{@bg_color}"
+  bg_api_url = "https://www.thecolorapi.com/id?hex=#{bg_color}"
   bg_api_response = HTTP.get(bg_api_url)
   bg_api_parsed = JSON.parse(bg_api_response)
 
@@ -49,7 +63,8 @@ get("/check_contrast") do
   @bg_name = bg_api_parsed["name"]["value"]
   @bg_contrast_value = bg_api_parsed["contrast"]["value"]
 
-  webaim_api_url = "https://webaim.org/resources/contrastchecker/?fcolor=#{@fg_color}&bcolor=#{@bg_color}&api"
+  # Request contrast checker api to get contrast ratio and pass/fail statements for AA and AAA for normal and large text 
+  webaim_api_url = "https://webaim.org/resources/contrastchecker/?fcolor=#{fg_color}&bcolor=#{bg_color}&api"
   webaim_api_response = HTTP.get(webaim_api_url)
   webaim_api_parsed = JSON.parse(webaim_api_response)
 
@@ -58,7 +73,5 @@ get("/check_contrast") do
   @aa_large = webaim_api_parsed["AALarge"].to_s
   @aaa = webaim_api_parsed["AAA"].to_s
   @aaa_large = webaim_api_parsed["AAALarge"].to_s
-  
 
-  erb(:home)
 end
